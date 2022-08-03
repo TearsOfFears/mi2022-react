e5d74702c74e52196a6e3396924ca24da8be8025
Nazar <nazardemchan@gmail.com>
2022-08-03 23:53:37 +0300

    added voting

--- src/Components/Vote/Vote.tsx
+++ src/Components/Vote/Vote.tsx
@@ -1,4 +1,4 @@
-import { Grid, Input, Paper } from "@mui/material";
+import { Grid, Input, Paper, Typography } from "@mui/material";
 import React from "react";
 import { SideBlockLayout } from "../../Layouts/SideBlockLayout";
 import ButtonIcon from "../ButtonIcon/ButtonIcon";
@@ -8,14 +8,122 @@ import { ReactComponent as BackArrow } from "./../../assets/icons/backArrow.svg"
 import { Button } from "../Button/Button";
 import { theme } from "../../theme";
 import Controls from "../Controls/Controls";
-
-
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { breedsService } from "../../query/breeds.service";
+import { ReactComponent as Fav } from "./../../assets/icons/fav.svg";
+import { ReactComponent as Like } from "./../../assets/icons/like.svg";
+import { ReactComponent as Dislike } from "./../../assets/icons/dislike.svg";
+import moment from "moment";
+interface RandomCat {
+    id?: string;
+    url?: string;
+}
 const Vote = () => {
     const handleClick = () => {};
+    const { data, isFetching } = useQuery<RandomCat[] | any>(
+        ["get random cat"],
+        () => breedsService.getRandomImageVote()
+    );
+
+    const makeVotes = useMutation(["make Vote"], breedsService.makeVote);
+    const getVotes = useQuery(["get votes"], breedsService.getVotes);
+    const handleVote = async (val: number, id?: string) => {
+        const data = {
+            image_id: id,
+            value: val
+        };
+        await makeVotes.mutateAsync({ data: data });
+    };
+    console.log(getVotes.data);
 
     return (
         <SideBlockLayout>
-                <Controls voting={true}/>
+            <>
+                <Controls voting={true} />
+                <Grid container className={styles.root}>
+                    <Grid item xs={12} style={{ width: "100%" }}>
+                        {!isFetching ? (
+                            <div className={styles.wrapperVote}>
+                                {data.map((obj: RandomCat, key: number) => {
+                                    return (
+                                        <div
+                                            key={key}
+                                            style={{
+                                                width: "100%",
+                                                display: "flex",
+                                                justifyContent: "center"
+                                            }}
+                                        >
+                                            <img src={obj?.url}></img>
+                                            <div
+                                                className={
+                                                    styles.wrapperIconsVoting
+                                                }
+                                            >
+                                                <button
+                                                    onClick={(e) =>
+                                                        handleVote(1, obj?.id)
+                                                    }
+                                                >
+                                                    <Like />
+                                                </button>
+                                                <button>
+                                                    <Fav />
+                                                </button>
+                                                <button
+                                                    onClick={(e) =>
+                                                        handleVote(-1, obj?.id)
+                                                    }
+                                                >
+                                                    <Dislike />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        ) : (
+                            <h1>loading...</h1>
+                        )}
+                    </Grid>
+                    <Grid item xs={12} marginTop="15px">
+                        {!getVotes.isFetching ? (
+                            getVotes.data.map((data: any, key: number) => {
+                                return (
+                                    <div key={key} className={styles.history}>
+                                        <span className={styles.time}>
+                                            {moment(data?.created_at).format(
+                                                "H:MM"
+                                            )}
+                                        </span>
+                                        <Typography variant="h5">
+                                            Image ID:{" "}
+                                            <span>{data?.image_id} </span>
+                                            was added to{" "}
+                                            {data?.value === 1 ? (
+                                                <>Likes</>
+                                            ) : (
+                                                <>Dislike</>
+                                            )}
+                                        </Typography>
+                                        {data?.value === 1 ? (
+                                            <>
+                                                <Like className={styles.likes}/>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <Dislike className={styles.disLikes}/>
+                                            </>
+                                        )}
+                                    </div>
+                                );
+                            })
+                        ) : (
+                            <h1>loading...</h1>
+                        )}
+                    </Grid>
+                </Grid>
+            </>
         </SideBlockLayout>
     );
 };
--- src/query/breeds.service.ts
+++ src/query/breeds.service.ts
@@ -6,6 +6,9 @@ interface breedIdProps {
     order?: any;
     id?: string;
 }
+interface breedVoteProps{
+   data?:any;
+}
 
 export const breedsService = {
     async getAllCats() {
@@ -23,11 +26,11 @@ export const breedsService = {
     async getImagesForCurrentCat(id: any) {
         let query_params = {
             breed_id: id,
-            limit:5,
+            limit: 5,
             has_breeds: 1
         };
         return await instance
-            .get(`/images/search`,{params:query_params})
+            .get(`/images/search`, { params: query_params })
             .then((res: AxiosResponse) => {
                 return res.data;
             });
@@ -44,7 +47,32 @@ export const breedsService = {
             .then((res: AxiosResponse) => {
                 return res.data;
             });
+    },
+    async getRandomImageVote() {
+        return await instance
+            .get(`/images/search`)
+            .then((res: AxiosResponse) => {
+                return res.data;
+            });
+    },
+    async getVotes() {
+        return await instance
+            .get(`/votes?limit=4&order=DESC`)
+            .then((res: AxiosResponse) => {
+                return res.data;
+            });
+    },
+    async makeVote({data}: breedVoteProps) {
+        console.log("data",data);
+        
+        return await instance
+            .post(`/votes`, data)
+            .then((res: AxiosResponse) => {
+                return res.data;
+            });
     }
+
+    //
     // async deleteComment(id){
     //     return await axios.get(`/commentDelete/${id}`)
     // },
